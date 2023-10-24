6edca73e70949e81306450c3fc5e019688544500
inane-phe <127912578+inane-phe@users.noreply.github.com>
2023-10-24 17:02:58 +0800

    Add files via upload

---
+++ GraphEditing/GraphEditing/Form1.Designer.cs
@@ -0,0 +1,214 @@
+﻿namespace GraphEditing
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器
+        /// 修改這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.LoadImageBtn = new System.Windows.Forms.Button();
+            this.flipHorizontalBtn = new System.Windows.Forms.Button();
+            this.flipVerticalBtn = new System.Windows.Forms.Button();
+            this.rotateBtn = new System.Windows.Forms.Button();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.clearButton = new System.Windows.Forms.Button();
+            this.colorPicker = new System.Windows.Forms.PictureBox();
+            this.cD = new System.Windows.Forms.ColorDialog();
+            this.previousImageBtn = new System.Windows.Forms.Button();
+            this.nextImsgeBtn = new System.Windows.Forms.Button();
+            this.imagePicturebox = new System.Windows.Forms.PictureBox();
+            this.imageGroupBox = new System.Windows.Forms.GroupBox();
+            this.saveBtn = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.colorPicker)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imagePicturebox)).BeginInit();
+            this.imageGroupBox.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // LoadImageBtn
+            // 
+            this.LoadImageBtn.Location = new System.Drawing.Point(174, 60);
+            this.LoadImageBtn.Name = "LoadImageBtn";
+            this.LoadImageBtn.Size = new System.Drawing.Size(85, 23);
+            this.LoadImageBtn.TabIndex = 0;
+            this.LoadImageBtn.Text = "Load Image";
+            this.LoadImageBtn.UseVisualStyleBackColor = true;
+            this.LoadImageBtn.Click += new System.EventHandler(this.LoadImageBtn_Click);
+            // 
+            // flipHorizontalBtn
+            // 
+            this.flipHorizontalBtn.Location = new System.Drawing.Point(174, 89);
+            this.flipHorizontalBtn.Name = "flipHorizontalBtn";
+            this.flipHorizontalBtn.Size = new System.Drawing.Size(85, 23);
+            this.flipHorizontalBtn.TabIndex = 1;
+            this.flipHorizontalBtn.Text = "flip Horizontal";
+            this.flipHorizontalBtn.UseVisualStyleBackColor = true;
+            this.flipHorizontalBtn.Click += new System.EventHandler(this.flipHorizontalBtn_Click);
+            // 
+            // flipVerticalBtn
+            // 
+            this.flipVerticalBtn.Location = new System.Drawing.Point(174, 118);
+            this.flipVerticalBtn.Name = "flipVerticalBtn";
+            this.flipVerticalBtn.Size = new System.Drawing.Size(85, 23);
+            this.flipVerticalBtn.TabIndex = 2;
+            this.flipVerticalBtn.Text = "flip Vertical";
+            this.flipVerticalBtn.UseVisualStyleBackColor = true;
+            this.flipVerticalBtn.Click += new System.EventHandler(this.flipVerticalBtn_Click);
+            // 
+            // rotateBtn
+            // 
+            this.rotateBtn.Location = new System.Drawing.Point(174, 147);
+            this.rotateBtn.Name = "rotateBtn";
+            this.rotateBtn.Size = new System.Drawing.Size(85, 23);
+            this.rotateBtn.TabIndex = 3;
+            this.rotateBtn.Text = "rotate";
+            this.rotateBtn.UseVisualStyleBackColor = true;
+            this.rotateBtn.Click += new System.EventHandler(this.rotateBtn_Click);
+            // 
+            // clearButton
+            // 
+            this.clearButton.Location = new System.Drawing.Point(174, 362);
+            this.clearButton.Name = "clearButton";
+            this.clearButton.Size = new System.Drawing.Size(85, 23);
+            this.clearButton.TabIndex = 4;
+            this.clearButton.Text = "clear";
+            this.clearButton.UseVisualStyleBackColor = true;
+            this.clearButton.Click += new System.EventHandler(this.clearButton_Click);
+            // 
+            // colorPicker
+            // 
+            this.colorPicker.Location = new System.Drawing.Point(174, 244);
+            this.colorPicker.Name = "colorPicker";
+            this.colorPicker.Size = new System.Drawing.Size(85, 76);
+            this.colorPicker.TabIndex = 5;
+            this.colorPicker.TabStop = false;
+            this.colorPicker.Click += new System.EventHandler(this.colorPicker_Click);
+            // 
+            // previousImageBtn
+            // 
+            this.previousImageBtn.Location = new System.Drawing.Point(69, 476);
+            this.previousImageBtn.Name = "previousImageBtn";
+            this.previousImageBtn.Size = new System.Drawing.Size(75, 23);
+            this.previousImageBtn.TabIndex = 6;
+            this.previousImageBtn.Text = "previous";
+            this.previousImageBtn.UseVisualStyleBackColor = true;
+            this.previousImageBtn.Click += new System.EventHandler(this.previousImageBtn_Click);
+            // 
+            // nextImsgeBtn
+            // 
+            this.nextImsgeBtn.Location = new System.Drawing.Point(438, 476);
+            this.nextImsgeBtn.Name = "nextImsgeBtn";
+            this.nextImsgeBtn.Size = new System.Drawing.Size(75, 23);
+            this.nextImsgeBtn.TabIndex = 7;
+            this.nextImsgeBtn.Text = "next";
+            this.nextImsgeBtn.UseVisualStyleBackColor = true;
+            this.nextImsgeBtn.Click += new System.EventHandler(this.nextImsgeBtn_Click);
+            // 
+            // imagePicturebox
+            // 
+            this.imagePicturebox.Location = new System.Drawing.Point(69, 21);
+            this.imagePicturebox.Name = "imagePicturebox";
+            this.imagePicturebox.Size = new System.Drawing.Size(444, 340);
+            this.imagePicturebox.TabIndex = 8;
+            this.imagePicturebox.TabStop = false;
+            this.imagePicturebox.MouseDown += new System.Windows.Forms.MouseEventHandler(this.imagePicturebox_MouseDown);
+            this.imagePicturebox.MouseMove += new System.Windows.Forms.MouseEventHandler(this.imagePicturebox_MouseMove);
+            this.imagePicturebox.MouseUp += new System.Windows.Forms.MouseEventHandler(this.imagePicturebox_MouseUp);
+            // 
+            // imageGroupBox
+            // 
+            this.imageGroupBox.Controls.Add(this.imagePicturebox);
+            this.imageGroupBox.Controls.Add(this.nextImsgeBtn);
+            this.imageGroupBox.Controls.Add(this.previousImageBtn);
+            this.imageGroupBox.Location = new System.Drawing.Point(298, 24);
+            this.imageGroupBox.Name = "imageGroupBox";
+            this.imageGroupBox.Size = new System.Drawing.Size(592, 515);
+            this.imageGroupBox.TabIndex = 9;
+            this.imageGroupBox.TabStop = false;
+            this.imageGroupBox.Text = "groupBox1";
+            // 
+            // saveBtn
+            // 
+            this.saveBtn.Location = new System.Drawing.Point(207, 176);
+            this.saveBtn.Name = "saveBtn";
+            this.saveBtn.Size = new System.Drawing.Size(85, 23);
+            this.saveBtn.TabIndex = 10;
+            this.saveBtn.Text = "save";
+            this.saveBtn.UseVisualStyleBackColor = true;
+            this.saveBtn.Click += new System.EventHandler(this.saveBtn_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(119, 232);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 12);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "pen color";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(930, 551);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.saveBtn);
+            this.Controls.Add(this.colorPicker);
+            this.Controls.Add(this.clearButton);
+            this.Controls.Add(this.rotateBtn);
+            this.Controls.Add(this.flipVerticalBtn);
+            this.Controls.Add(this.flipHorizontalBtn);
+            this.Controls.Add(this.LoadImageBtn);
+            this.Controls.Add(this.imageGroupBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.colorPicker)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imagePicturebox)).EndInit();
+            this.imageGroupBox.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button LoadImageBtn;
+        private System.Windows.Forms.Button flipHorizontalBtn;
+        private System.Windows.Forms.Button flipVerticalBtn;
+        private System.Windows.Forms.Button rotateBtn;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Button clearButton;
+        private System.Windows.Forms.PictureBox colorPicker;
+        private System.Windows.Forms.ColorDialog cD;
+        private System.Windows.Forms.Button previousImageBtn;
+        private System.Windows.Forms.Button nextImsgeBtn;
+        private System.Windows.Forms.PictureBox imagePicturebox;
+        private System.Windows.Forms.GroupBox imageGroupBox;
+        private System.Windows.Forms.Button saveBtn;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ GraphEditing/GraphEditing/Form1.cs
@@ -0,0 +1,271 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GraphEditing
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private List<Bitmap> imageList = new List<Bitmap>(); //圖片存在這裡
+        private List<Bitmap> originalImageList = new List<Bitmap>();//備份圖片
+        private int currentImageIndex = 0;
+
+        private Size defaultImagePicturebox; //記住預設的大小
+        private float minScaleFator = 0.1f;
+        private float maxScaleFator = 2.0f;
+        //畫圖
+        private bool isDrawing = false;
+        private Point previousPoint;
+
+        private Pen currentPen = new Pen(Color.Black, 2); //初始為黑
+
+        Point originalCenter;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Width = 1000;
+            this.Height = 600;
+
+            colorPicker.BackColor = Color.Black;
+
+            defaultImagePicturebox = imagePicturebox.Size;
+
+            imageGroupBox.MouseWheel += new MouseEventHandler(PictureBox1_MouseWheel);
+
+            originalCenter = new Point(imageGroupBox.Width / 2, imageGroupBox.Height / 2);
+
+
+        }
+        private Bitmap ResizeImage(Image image, int width, int height)
+        {
+            Bitmap resizedImage = new Bitmap(width, height);
+
+            Graphics g = Graphics.FromImage(resizedImage);
+            g.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.HighQualityBicubic;
+            g.DrawImage(image, 0, 0, width, height);
+            
+            return resizedImage;
+        }
+        private void PictureBox1_MouseWheel(object sender, MouseEventArgs e)
+        {
+            float scaleFactor = 1.1f;
+            float newWidth =    imagePicturebox.Width;
+            float newHeight =   imagePicturebox.Height;
+            if(e.Delta >0 && newWidth <= originalImageList[currentImageIndex].Width * maxScaleFator && newHeight <= originalImageList[currentImageIndex].Height * maxScaleFator)//delta屬於mouseEvent
+            {
+                imagePicturebox.Width = (int)(imagePicturebox.Width * scaleFactor);
+                imagePicturebox.Height = (int)(imagePicturebox.Height * scaleFactor);
+            }
+            else if (e.Delta < 0 && newWidth >= originalImageList[currentImageIndex].Width * maxScaleFator && newHeight >= originalImageList[currentImageIndex].Height * maxScaleFator)
+            {
+                imagePicturebox.Width = (int)(imagePicturebox.Width / scaleFactor);
+                imagePicturebox.Height = (int)(imagePicturebox.Height / scaleFactor);
+            }
+
+            imagePicturebox.Left = originalCenter.X - imagePicturebox.Width / 2;
+            imagePicturebox.Top = originalCenter.Y - imagePicturebox.Height / 2;
+
+            imagePicturebox.Image = ResizeImage(imageList[currentImageIndex], imagePicturebox.Width, imagePicturebox.Height);
+
+            //強制 imagePicturebox 重新繪製以顯示縮放效果
+            imagePicturebox.Invalidate();
+        }
+        private void LoadImageBtn_Click(object sender, EventArgs e)
+        {
+            //打開文件對話框以選擇多個圖像文件
+            using(OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.Filter = "圖像文件|*.jpg;*.jpeg;*.png;*.gif;*.bmp";
+                openFileDialog.Multiselect = true; //允許多選
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    foreach(string fileName in openFileDialog.FileNames)
+                    {
+                        //讀取所選圖像文件添加到圖片清單中
+                        Bitmap newImage = new Bitmap(fileName);
+                        imageList.Add(newImage);
+
+                        //備份原來的圖片
+                        originalImageList.Add(newImage);
+                    }
+                    
+                }
+                //顯示第一張圖片
+                currentImageIndex = 0;
+                ShowCurrentImage();
+            }
+        }
+        private void ShowCurrentImage()
+        {
+            if (imageList.Count > 0 && currentImageIndex >= 0 && currentImageIndex < imageList.Count)
+            { 
+                float widthRatio = (float)imageList[currentImageIndex].Width / defaultImagePicturebox.Width;
+                float heightRatio = (float)imageList[currentImageIndex].Height / defaultImagePicturebox.Height;
+                float MaxRatio = Math.Max(widthRatio, heightRatio);
+
+                imagePicturebox.Width = (int)(imageList[currentImageIndex].Width / MaxRatio);
+                imagePicturebox.Height = (int)(imageList[currentImageIndex].Width / MaxRatio);
+
+                imagePicturebox.Image = ResizeImage(imageList[currentImageIndex], imagePicturebox.Width, imagePicturebox.Height);
+
+                Point originalCenter = new Point(imageGroupBox.Width / 2, imageGroupBox.Height / 2);
+                imagePicturebox.Left = originalCenter.X - imagePicturebox.Width / 2;
+                imagePicturebox.Top = originalCenter.Y - imagePicturebox.Height / 2;
+            }
+        }
+
+        private void previousImageBtn_Click(object sender, EventArgs e)
+        {
+            //切換到上一張圖片
+            currentImageIndex--;
+            if (currentImageIndex < 0)
+            {
+                currentImageIndex = imageList.Count - 1;    //循環顯示
+            }
+            ShowCurrentImage();
+        }
+
+        private void nextImsgeBtn_Click(object sender, EventArgs e)
+        {
+            //切換到下一張圖片
+            currentImageIndex++;
+            if (currentImageIndex >= imageList.Count)
+            {
+                currentImageIndex = 0;                      //循環顯示
+            }
+            ShowCurrentImage();
+        }
+
+        private void saveBtn_Click(object sender, EventArgs e)
+        {
+            //存檔
+            if (imagePicturebox.Image != null)
+            {
+                using (SaveFileDialog saveFileDialog = new SaveFileDialog())
+                {
+                    saveFileDialog.Filter = "JPEG圖像|*.jpg";
+                    if (saveFileDialog.ShowDialog() == DialogResult.OK)
+                    {
+                        imagePicturebox.Image.Save(saveFileDialog.FileName);
+                    }
+                }
+            }
+        }
+
+        private void clearButton_Click(object sender, EventArgs e)
+        {
+            if (imagePicturebox.Image != null)
+            {
+                imagePicturebox.Image = originalImageList[currentImageIndex];
+                MessageBox.Show("已還原圖片");
+            }
+        }
+
+        private void flipHorizontalBtn_Click(object sender, EventArgs e)
+        {
+            if (imagePicturebox.Image != null)
+            {
+                imagePicturebox.Image.RotateFlip(RotateFlipType.RotateNoneFlipX);
+
+                imagePicturebox.Image = imagePicturebox.Image;
+
+                imageList[currentImageIndex] = new Bitmap(imagePicturebox.Image);
+
+
+            }
+        }
+
+        private void flipVerticalBtn_Click(object sender, EventArgs e)
+        {
+            if (imagePicturebox.Image != null)
+            {
+                imagePicturebox.Image.RotateFlip(RotateFlipType.RotateNoneFlipY);
+
+                imagePicturebox.Image = imagePicturebox.Image;
+
+                imageList[currentImageIndex] = new Bitmap(imagePicturebox.Image);
+            }
+        }
+
+        private void rotateBtn_Click(object sender, EventArgs e)
+        {
+            //旋轉
+            if (imagePicturebox.Image != null)
+            {
+                imagePicturebox.Image.RotateFlip(RotateFlipType.Rotate90FlipY);
+
+                imagePicturebox.Image = imagePicturebox.Image;
+
+                //把翻轉過來的圖片覆蓋原本的圖片
+                imageList[currentImageIndex] = new Bitmap(imagePicturebox.Image);
+            }
+            ShowCurrentImage();
+        }
+
+        private void imagePicturebox_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (imagePicturebox.Image != null)
+            {
+                isDrawing = true;
+
+                //紀錄滑鼠座標
+                previousPoint = e.Location;
+            }
+        }
+
+        private void imagePicturebox_MouseUp(object sender, MouseEventArgs e)
+        {
+            isDrawing = false;
+            
+        }
+
+        private void imagePicturebox_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (isDrawing && imagePicturebox.Image != null)
+            {
+                //創建一個新的Bitmap 將圖片複製過去
+                Bitmap bitmap = new Bitmap(imagePicturebox.Image);
+
+                //繪製線條
+                Graphics g = Graphics.FromImage(bitmap);
+                g.DrawLine(currentPen, previousPoint.X, previousPoint.Y, e.X, e.Y);
+
+                //將新的Bitmap設定為PictureBox的圖片
+                imagePicturebox.Image = bitmap;
+
+                //更新原來的圖片
+                imageList[currentImageIndex] = bitmap;
+
+                //強制 imagePicturebox 重新繪製以顯示線條
+                imagePicturebox.Invalidate();
+
+                //紀錄滑鼠座標
+                previousPoint = e.Location;
+            }
+        }
+
+        private void colorPicker_Click(object sender, EventArgs e)
+        {
+            if (cD.ShowDialog() == DialogResult.OK)
+            {
+                colorPicker.BackColor = cD.Color;
+            }  
+
+        }
+   
+        
+    
+    
+    
+    }
+    }
